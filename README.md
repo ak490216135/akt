3928a3040453c6083215ca010ddf1e8027329da7
ak490216135 <490216135@qq.com>
2022-04-13 22:24:16 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,1 @@
+Goerli-AKT:https://goerli.etherscan.io/address/0x1d73310380aF4EC1E5c4013EC17a69C5aA52df0D
---
+++ contract/Ak490216135Token.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.5.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.5.0/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.5.0/security/Pausable.sol";
+import "@openzeppelin/contracts@4.5.0/access/Ownable.sol";
+
+/// @custom:security-contact 490216135@qq.com
+contract Ak490216135Token is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("ak490216135 Token", "AKT") {
+        _mint(msg.sender, 100 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
